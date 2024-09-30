8b4ee24972d24830a579e32bbdac00c3329efcc2
Hoeltgebaum, Daniel (SE-A/31) <daniel.hoeltgebaum@volkswagen.de>
2024-09-30 16:30:34 +0200

    Initial Commit

---
+++ Notizen/NotizenApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotizenApp.swift
+//  Notizen
+//
+//  Created by Hoeltgebaum, Daniel (SE-A/32) on 30.09.24.
+//
+
+import SwiftUI
+
+@main
+struct NotizenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
